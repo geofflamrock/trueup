cd9c59ad44f77f60edfe0e96f71bfe53ce894ea0
Geoff Lamrock <geoff.lamrock@gmail.com>
2026-03-08 16:49:09 +1100

    Try without sw

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
-import netlify from "@netlify/vite-plugin";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
@@ -12,10 +11,9 @@ export default defineConfig({
     reactRouter(),
     tsconfigPaths(),
     netlifyReactRouter(),
-    // netlify(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: "auto",
+      injectRegister: false,
       includeAssets: ["favicon.ico"],
       manifest: {
         id: "true-up",
@@ -56,39 +54,39 @@ export default defineConfig({
           },
         ],
       },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "gstatic-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
+      // workbox: {
+      //   globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
+      //   runtimeCaching: [
+      //     {
+      //       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+      //       handler: "CacheFirst",
+      //       options: {
+      //         cacheName: "google-fonts-cache",
+      //         expiration: {
+      //           maxEntries: 10,
+      //           maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+      //         },
+      //         cacheableResponse: {
+      //           statuses: [0, 200],
+      //         },
+      //       },
+      //     },
+      //     {
+      //       urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+      //       handler: "CacheFirst",
+      //       options: {
+      //         cacheName: "gstatic-fonts-cache",
+      //         expiration: {
+      //           maxEntries: 10,
+      //           maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+      //         },
+      //         cacheableResponse: {
+      //           statuses: [0, 200],
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     }),
   ],
 });
